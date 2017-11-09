0a6f195c700289d832b3c1bf67a2698ed5d897da
alexandraalday <alexandra.alday@gmail.com>
2017-11-08 22:02:04 -0800

    initial setup

---
+++ Volta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Volta
+//
+//  Created by Alexandra Alday on 11/8/17.
+//  Copyright © 2017 Alexandra Alday. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
